6ebf8ea425b08bade5a86681ba7b2b528432956e
Berry  Semexan <bsemexan@gmail.com>
2018-04-05 17:41:32 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
-
   validates_presence_of :name, :content
   accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes[:name].blank? }
 end
