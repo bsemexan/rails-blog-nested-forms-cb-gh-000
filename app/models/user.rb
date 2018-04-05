28ef7ebb53e311606cd3dcf59ce38d77d80df83a
Berry  Semexan <bsemexan@gmail.com>
2018-04-05 16:54:31 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,4 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
 
-  # accepts_nested_attributes_for :posts, reject_if: proc { |attributes| attributes['title'].blank? }
-  # end
 end
